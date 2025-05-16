f841885af9f43fadd267fa7c37d5234d9e58d7a9
Neha-Himesh <neha.himesh@spike.sh>
2025-05-16 17:45:11 +0530

    Initial code without lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
+import Name from './Name';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Happy Birthday!</h1>
+      <Message message="May you shine as bright as sun!" />
+      <Name name='Neha' />
     </div>
   );
 }
---
+++ src/Name.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Name({name}) {
+  return (
+    <div>
+        <p>By {name}</p>
+    </div>
+  )
+}
+
+export default Name
